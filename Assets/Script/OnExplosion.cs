8ec1638f0bf50e2eb0ac902f505addebf83d2d52
lcs19863 <lcs19863@gmail.com>
2018-05-04 14:53:31 -0400

    Initial commit

---
+++ Assets/Script/OnExplosion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnExplosion : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        col.gameObject.SendMessage("minusHealth",2000f);
+
+            Debug.Log("sent!");
+        
+    }
+}
---
+++ Assets/Script/Score.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Score : MonoBehaviour {
+
+    public int score = 3000;
+    public GameObject scoreDisplay;
+
+	// Use this for initialization
+	void Start () {
+        scoreDisplay.GetComponent<Text>().text = "Score:" + score;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        scoreDisplay.GetComponent<Text>().text = "Score:" + score;
+
+    }
+
+    public void changeScore(int i)
+    {
+        score = score + i;
+    }
+}
